6307ccf9455c0c2616236600c89e2d6b3b46d79f
Maxyee <eyaminkhan00@gmail.com>
2018-08-24 18:09:02 +0600

    Added all the functionality for the game

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
+window.addEventListener('load',init)
+
+
+//Available Levels
+const levels = {
+    easy:5,
+    medium:3,
+    hard: 2
+}
+
+//To change level
+const currentLevel = levels.easy;
+
+
 // Global variables
-let time = 5;
+let time = currentLevel;
 let score = 0;
 let isPlaying;
 
@@ -11,3 +25,129 @@ const scoreDisplay = document.querySelector('#score');
 const timeDisplay = document.querySelector('#time');
 const message = document.querySelector('#message');
 const seconds = document.querySelector('#seconds');
+
+
+const words = [
+    'hat',
+    'river',
+    'lucky',
+    'statue',
+    'generate',
+    'stubborn',
+    'cocktail',
+    'runaway',
+    'joke',
+    'developer',
+    'establishment',
+    'hero',
+    'javascript',
+    'nutrition',
+    'revolver',
+    'echo',
+    'siblings',
+    'investigate',
+    'horrendous',
+    'symptom',
+    'laughter',
+    'magic',
+    'master',
+    'space',
+    'definition'
+];
+
+
+//initiazling the game functions
+function init()
+{
+    //show numnber of second in UI
+    seconds.innerHTML = currentLevel;
+    //Load random word form the array
+    showWord(words);
+
+    //Start matching on word input
+    wordInput.addEventListener('input',startMatch);
+
+    //call countdown every second
+    setInterval(countdown, 1000);
+
+    //check game status
+    setInterval(checkStatus, 50);
+}
+
+//Start match
+function startMatch()
+{
+    if(matchWords())
+    {
+        isPlaying = true;
+        time = currentLevel + 1;
+        showWord(words);
+        wordInput.value = '';
+        score++;
+    }
+
+    //if score -1 , display 0
+    if(score === -1)
+    {
+        scoreDisplay.innerHTML = 0;
+    }
+    else
+    {
+        scoreDisplay.innerHTML = score;
+    }
+    
+}
+
+
+//Match currentrWord to wordInput
+function matchWords()
+{
+    if(wordInput.value === currentWord.innerHTML)
+    {
+        message.innerHTML = 'Correct !!!';
+        return true;
+    }
+    else
+    {
+        message.innerHTML = '';
+        return false;
+    }
+}
+
+//Pick and show random word 
+function showWord(words)
+{
+    //Generate random array index
+    const randIndex = Math.floor(Math.random() * words.length);
+    //Output random word
+    currentWord.innerHTML = words[randIndex];
+}
+
+//Countdown timer
+function countdown()
+{
+    //check time is run out
+    if(time > 0)
+    {
+        //decrease the time
+        time--;
+    }
+    else if(time === 0)
+    {
+        //game is over
+        isPlaying = false;
+    }
+    //Show time
+    timeDisplay.innerHTML = time;
+}
+
+
+//Check game status
+function checkStatus()
+{
+    if(!isPlaying && time === 0)
+    {
+        message.innerHTML = 'Game Over!!!';
+        score = -1;
+    }
+}
