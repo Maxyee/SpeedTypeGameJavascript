ef58d28e244a17b829a77e3a5394dd5ef119b2f3
Maxyee <eyaminkhan00@gmail.com>
2018-08-24 17:14:10 +0600

    Added nessary file for this speed type project

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// Global variables
+let time = 5;
+let score = 0;
+let isPlaying;
+
+
+//Dom elements
+const wordInput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
